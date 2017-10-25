6eee1acfb459e063dd8b54d18b05294606746e09
whynot-s <whynot_s@outlook.com>
2017-10-25 17:11:30 +0800

    完成Team中任意两人亲密度计算，利用广度优先计算得到；测试正确

--- src/main/java/cn/springmvc/service/CompetitorIntimacy.java
+++ src/main/java/cn/springmvc/service/CompetitorIntimacy.java
@@ -1,9 +1,12 @@
 package cn.springmvc.service;
 
 import cn.springmvc.dao.RelationGenDao;
+import cn.springmvc.dao.IntimacyDao;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,9 @@ public class CompetitorIntimacy {
 
 	@Autowired
 	private RelationGenDao relationGenDao;
+	
+	@Autowired
+	private IntimacyDao intimacyDao;
 	
 	//计算两用户间亲密度并加入数据库中
 	public void pairIntimacy() {
@@ -51,6 +57,98 @@ public class CompetitorIntimacy {
 		return 1.0/sum;
 	}
 
+	//初始化操作时将TeamMemberIntimacy清空(若不存在则新建表)，并根据CompetitionLeaderBoard中的队伍信息按队伍每两个人加入记录，并加入两人团队中亲密度
+	public void genTeamIntimacy() {
+		intimacyDao.truncateTeamIntimacy();
+		List<Map<String, Object>> teams = intimacyDao.getTeams();
+		for(Map<String, Object> team : teams) {
+			int teamId = (Integer)(team.get("teamId"));
+			String[] teamMembers = ((String)team.get("memberIds")).split("&");
+			if(teamMembers.length > 1) {
+				//初始化Team矩阵
+				double[][] graphWeights = new double[teamMembers.length][teamMembers.length];
+				loadGraphWeights(teamMembers, graphWeights);
+				//计算Team中任意两个人的亲密度
+				double[][] teamIntimacy = new double[teamMembers.length][teamMembers.length];
+				calcTeamIntimacy(graphWeights, teamIntimacy);
+				//将Team中任意两人亲密度加入TeamMemberIntimacy中
+				for(int i = 0; i < teamMembers.length; i++) {
+					int competitorId1 = Integer.parseInt(teamMembers[i]);
+					for(int j = i + 1; j < teamMembers.length; j++) {
+						int competitorId2 = Integer.parseInt(teamMembers[j]);
+						intimacyDao.insertTeamPair(teamId, competitorId1, competitorId2);
+						intimacyDao.insertTeamPair(teamId, competitorId2, competitorId1);
+						intimacyDao.updateTeamPairIntimacy(teamIntimacy[i][j], teamId, competitorId1, competitorId2);
+						intimacyDao.updateTeamPairIntimacy(teamIntimacy[j][i], teamId, competitorId2, competitorId1);
+					}
+				}
+			}else {
+				continue;
+			}
+		}
+	}	
+
+	//根据TeamemberIds将CompetitorIntimacy中的两者亲密度取出并存入GraphWeights中
+	public void loadGraphWeights(String[] teamMembers, double[][] graphWeights) {
+		for(int i = 0; i < teamMembers.length; i++) {
+			graphWeights[i] = new double[teamMembers.length];
+			int competitorId1 = Integer.parseInt(teamMembers[i]);
+			for(int j = 0; j < teamMembers.length; j++) {
+				if(i == j) {
+					//自己和自己的亲密度设置为0
+					graphWeights[i][j] = 0.0;
+				}else {
+					int competitorId2 = Integer.parseInt(teamMembers[j]);
+					Double weights = intimacyDao.getPairIntimacy(competitorId1, competitorId2);
+					//若两个人亲密度在表中无记录，则设置为-1.0
+					graphWeights[i][j] = weights == null? -1.0 : weights;
+				}
+			}
+		}
+	}
+
+	//根据已读入的带权图计算每个Team中任意两个人的亲密度，并存入TeamIntimacy中
+	public void calcTeamIntimacy(double[][] graphWeights, double[][] TeamIntimacy) {
+		for(int i = 0; i < graphWeights.length; i++) {
+			TeamIntimacy[i] = new double[graphWeights.length];
+			for(int j = 0; j < graphWeights.length; j++) {
+				//遍历任意两个Team members
+				if(i == j) {
+					TeamIntimacy[i][j] = 0.0;
+				}else {
+					TeamIntimacy[i][j] = shortestWeightedPath(i, j, graphWeights);
+				}
+			}
+		}
+	}
 	
-	
+	//广度优先计算start-->end的最短加权路径，权值为方阵表示
+	public double shortestWeightedPath(int start, int end, double[][] weights) {
+		double minWeight = Double.MAX_VALUE;
+		Queue<Integer> subNodes = new LinkedList<Integer>();
+		Queue<Double> minWeights = new LinkedList<Double>();
+		subNodes.offer(start);
+		minWeights.offer(0.0);
+		while(!subNodes.isEmpty()) {
+			int currentNode = subNodes.poll();
+			double currentWeight = minWeights.poll();
+			for(int i = 0; i < weights[currentNode].length; i++) {
+				//回头路不走，不通的路不走，已经大于等于现有最小权值的路不走（不入队）
+				if(i == currentNode || weights[currentNode][i] == -1.0 || currentWeight >= minWeight) {
+					continue;
+				//遇终点判断权值大小，不入队
+				}else if(i == end){
+					if(currentWeight + weights[currentNode][i] < minWeight) {
+						minWeight = currentWeight + weights[currentNode][i];
+					}
+				//入队
+				}else {
+					subNodes.offer(i);
+					minWeights.offer(currentWeight + weights[currentNode][i]);
+				}
+			}
+		}
+		return minWeight;
+	}
+
 }
--- src/test/java/cn/springmvc/test/calculateIntimacyTest.java
+++ src/test/java/cn/springmvc/test/calculateIntimacyTest.java
@@ -1,5 +1,7 @@
 package cn.springmvc.test;
 
+import static org.junit.Assert.*;
+
 import cn.springmvc.service.CompetitorIntimacy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +19,30 @@ public class calculateIntimacyTest {
 	
 	@Test
 	public void test() {
-		competitorIntimacy.pairIntimacy();
+//		competitorIntimacy.pairIntimacy();
+		competitorIntimacy.genTeamIntimacy();
+	}
+	
+//	测试最短路径计算正确性
+	@Test
+	public void ShortestWeightedPathTest() {
+		double[][] weights = new double[4][4];
+		String W = "0.0;0.01;0.08;0.18;0.01;0.0;0.06;0.02;0.08;0.06;0.0;0.09;0.18;0.02;0.09;0.0";
+		double[][] intimacy = new double[4][4];
+		String K = "0.0;0.01;0.07;0.03;0.01;0.0;0.06;0.02;0.07;0.06;0.0;0.08;0.03;0.02;0.08;0.0";
+		String[] W_s = W.split(";");
+		String[] K_s = K.split(";");
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j ++) {
+				weights[i][j] = Double.parseDouble(W_s[i * 4 + j]);
+			}
+		}
+		competitorIntimacy.calcTeamIntimacy(weights, intimacy);
+		for(int i = 0; i < intimacy.length; i++){
+			for(int j = 0; j < intimacy[i].length; j++) {
+				assertEquals(Double.parseDouble(K_s[i * 4 + j]), intimacy[i][j], 0.00000000001);
+			}
+		}
 	}
 
 }
