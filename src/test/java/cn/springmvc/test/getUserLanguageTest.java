7b70e7d8b8d52ba2c7620a58c9dd30f9b4b9e72e
wutiange <a809472324@163.com>
2017-10-29 11:54:12 +0800

    测试一下提交的整个流程

--- src/test/java/cn/springmvc/test/getUserLanguageTest.java
+++ src/test/java/cn/springmvc/test/getUserLanguageTest.java
@@ -34,31 +34,38 @@ public class getUserLanguageTest {
     @Autowired
     private CompetitorAbilityDao competitorAbilityDao;
 
-    @Test
-    public void testGetLanguages() throws Exception {
-        //  getUserLanguage.getLanguages();
-        //   competitorAbility.containsGraph();
-        //competitorAbility.totalAbilityGen();
-        /*SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
-        String stringToDate1 = "7-30-2017";
-        String stringToDate2 = "8-30-2017";
-        Date date1 = dateFormat.parse(stringToDate1);
-        Date date2 = dateFormat.parse(stringToDate2);
-        int duration = (int) ((date2.getTime() - date1.getTime())/1000/60/60/24);
-        System.out.println(duration);*/
-        //System.out.println(50/500.0);
-        // competitiorRelation.testes();
-        System.out.println();
-        // competitiorRelation.insertCollaborationRelation();
-        //  competitorAbility.totalAbilityGen();
-        //  competitorAbility.test();
-    }
+//    @Test
+//    public void testGetLanguages() throws Exception {
+//        //  getUserLanguage.getLanguages();
+//        //   competitorAbility.containsGraph();
+//        //competitorAbility.totalAbilityGen();
+//        /*SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+//        String stringToDate1 = "7-30-2017";
+//        String stringToDate2 = "8-30-2017";
+//        Date date1 = dateFormat.parse(stringToDate1);
+//        Date date2 = dateFormat.parse(stringToDate2);
+//        int duration = (int) ((date2.getTime() - date1.getTime())/1000/60/60/24);
+//        System.out.println(duration);*/
+//        //System.out.println(50/500.0);
+//        // competitiorRelation.testes();
+//        System.out.println();
+//        // competitiorRelation.insertCollaborationRelation();
+//        //  competitorAbility.totalAbilityGen();
+//        //  competitorAbility.test();
+//    }
 
 
     @Test
-    public void main() {
+    public void main()
+    {
         String[] tags = new String[]{"image", "tabular", "binary classification", "totalScore"};
         recommend.getRecommendTeam(3757, 3, tags, 0.1, 0.3, 1.0, 0.1);
+    }
+
+    @Test
+    public void test()
+    {
+        System.out.println("test");
     }
 
 }
